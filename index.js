559803d2da16c1d3fb3db0c23ee81c36886e3c8b
FND <fnd@localhost.localdomain>
2015-12-29 12:41:13 +0100

    added settings handling, extracted from DOM attributes
    note that we're *not* using `data-*` attributes as that seems to reduce
    readability - thus technically our custom attributes are invalid (though
    `is` doesn't currently validate either)
    
    the actual settings are not being used yet, which is a bit YAGNI - but
    since we're rewriting an existing implementation, we kinda know what's
    coming (particularly WRT the underlying mechanics, i.e. defaults and
    type conversion)

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 /*eslint-env browser */
 /*global HTMLInputElement */
 
+const DEFAULTS = {
+	delay: 250,
+	minLength: 3,
+	itemSelector: "li",
+	selectedClass: "selected"
+};
+
 class SimpleteBox extends HTMLInputElement {
+	createdCallback() {
+		// parse settings from DOM
+		this.settings = {};
+		this.getSetting("delay", "delay", "integer");
+		this.getSetting("min-length", "minLength", "integer");
+		this.getSetting("item-selector", "itemSelector");
+		this.getSetting("selected-class", "selectedClass");
+	}
+
 	attachedCallback() {
 		if(this.wrapper) { // ignore second invocation (due to DOM manipulation below)
 			return;
@@ -14,6 +30,16 @@ class SimpleteBox extends HTMLInputElement {
 		// add results container
 		let results = createElement("div", "results");
 		container.appendChild(results);
+	}
+
+	getSetting(attr, name, type) {
+		let value = this.getAttribute(attr);
+		if(!value) {
+			value = DEFAULTS[name];
+		} else if(type === "integer") {
+			value = parseInt(value, 10);
+		}
+		this.settings[name] = value;
 	}
 }
 
--- tests/index.js
+++ tests/index.js
@@ -24,6 +24,25 @@ QUnit.test("DOM initialization", assert => {
 	assert.strictEqual(results.className, "results");
 });
 
+QUnit.test("settings", assert => {
+	let field = createElement(`<input is="simplete-box">`);
+	assert.deepEqual(field.settings, {
+		delay: 250,
+		minLength: 3,
+		itemSelector: "li",
+		selectedClass: "selected"
+	});
+
+	field = createElement(`<input is="simplete-box" delay="500"
+			item-selector="dt">`);
+	assert.deepEqual(field.settings, {
+		delay: 500,
+		minLength: 3,
+		itemSelector: "dt",
+		selectedClass: "selected"
+	});
+});
+
 function createElement(html) {
 	let container = document.createElement("div");
 	container.innerHTML = html;
